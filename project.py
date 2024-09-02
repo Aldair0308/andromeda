37b9036d7df9d9a674faa0ee5ffe32ee097b8d71
Aldairm0308 <al222111405@gmail.com>
2024-09-01 18:40:17 -0600

    local function added

--- project.py
+++ project.py
@@ -3,3 +3,5 @@ Este es lo que seria equivalente a el codigo de el proyecto
 Simbolicamente agregaremos libreberias necesarias para mi project
 
 Commit de main en Git Hub
+
+Commit de main en local (thinkpad 01)
