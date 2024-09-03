20b3aba75c76a9549cbeff05cd0da83fc9f3a9c1
Aldairm0308 <al222111405@gmail.com>
2024-09-03 10:34:32 -0600

    Revert "library actived"
    This reverts commit caeb33888e65edf03aa3e5345e163743d250bb99.

--- app.py
+++ app.py
@@ -2,4 +2,3 @@ Este es el primer codigo dentro de app.py
 Y deberiamos de seguir podiendo escribir codigo
 # Vamos agregar code desde el main
 # Vamos a seguir agregando mas code desde main
-# Vamos a editar esto en un dia distinto
